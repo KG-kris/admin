67e713a8f0a394246f1e01619ddadb5a248f1465
ai571581929 <571581929@qq.com>
2020-07-30 22:50:26 +0800

    更新端口路由

--- config/index.js
+++ config/index.js
@@ -12,10 +12,13 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       "/api":{
-        target:"http://10.10.44.210:3000",
+        // target:"http://10.10.44.210:3000",
+        target:"http://localhost:3000",
+        
         changeOrigin:true,
         pathRewrite:{
-          "^/api":"http://10.10.44.210:3000"
+          // "^/api":"http://10.10.44.210:3000"
+          "^/api":"http://localhost:3000"
         }
       }
     },
